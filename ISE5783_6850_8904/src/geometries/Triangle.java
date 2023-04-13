0c6c4f54d486b721b8dd0901336f02b1dc0d84fb
noam-ben-lulu <benlulu@yatmal.co.il>
2023-04-13 09:59:58 +0300

    a

--- ISE5783_6850_8904/src/geometries/Triangle.java
+++ ISE5783_6850_8904/src/geometries/Triangle.java
@@ -1,6 +1,8 @@
 package geometries;
 
 import primitives.Point;
+import primitives.Vector;
+
 /**
  * Triangle class represents a 3D triangle in Cartesian coordinate system.
  * A triangle is defined by its three vertices.
@@ -12,4 +14,6 @@ public class Triangle extends Polygon {
     public Triangle(Point p1,Point p2,Point p3) {
         super(p1,p2,p3);
     }
+
+
 }
