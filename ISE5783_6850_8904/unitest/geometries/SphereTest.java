b4dfb6d2c6d5f099ed99c48cc75590f199e15d66
noam-ben-lulu <benlulu@yatmal.co.il>
2023-04-13 13:26:09 +0300

    a

---
+++ ISE5783_6850_8904/unitest/geometries/SphereTest.java
@@ -0,0 +1,20 @@
+package geometries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SphereTest {
+
+    @Test
+    void getCenter() {
+    }
+
+    @Test
+    void getRadius() {
+    }
+
+    @Test
+    void getNormal() {
+    }
+}
